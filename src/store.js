33baf936f7cd657517582934479c214ed1453221
Rahul Chand <rahul.ec14@gmail.com>
2021-01-25 11:54:04 +0530

    Added redux-devtool

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { createStore } from "redux";
 import counterReducer from "./reducers/counter";
 
 let store = createStore(
-    counterReducer
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store;
